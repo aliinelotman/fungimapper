60bbfd036aaabc98c09c91b91d099638a4045514
Aliine Lotman <aliinelotman@gmail.com>
2023-09-27 22:34:06 +0300

    marker progress

--- frontend/angularclient/src/app/fungi-map/fungi-map.component.ts
+++ frontend/angularclient/src/app/fungi-map/fungi-map.component.ts
@@ -1,46 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { HostListener, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+declare var ol: any;
 @Component({
   selector: 'app-fungi-map',
   templateUrl: './fungi-map.component.html',
-  styleUrls: ['./fungi-map.component.css']
+  styleUrls: ['./fungi-map.component.css'],
 })
 export class FungiMapComponent {
-
   private map!: L.Map;
-  private centroid: L.LatLngExpression = [58.375759610207155, 26.669222616313476]; //tartu Ossu
+  private centroid: L.LatLngExpression = [
+    58.375759610207155, 26.669222616313476,
+  ]; //tartu Ossu
 
   private initMap(): void {
     this.map = L.map('map', {
       center: this.centroid,
-      zoom: 12
+      zoom: 12,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 10,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 10,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    this.map.on('click', (event: L.LeafletMouseEvent) => {
+      const latitude = event.latlng.lat;
+      const longitude = event.latlng.lng;
+      this.addMarkerToMap(latitude, longitude);
     });
-
-    // create 5 random jitteries and add them to map
-    const jittery = Array(5).fill(this.centroid).map(
-        x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]
-      ).map(
-        x => L.marker(x as L.LatLngExpression)
-      ).forEach(
-        x => x.addTo(this.map)
-      );
-
 
     tiles.addTo(this.map);
-
   }
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.initMap();
   }
 
+  addMarkerToMap(latitude: number, longitude: number): void {
+    const marker = L.marker([latitude, longitude], {
+      icon: this.fungiIcon,
+    }).addTo(this.map);
+    // Optionally, you can add the marker to a list or array for further use.
+  }
+
+  fungiIcon = L.icon({
+    iconUrl: '/mushroom-icon.png', //EI TÖÖTA, 404?
+    iconSize: [38, 95], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
 }
